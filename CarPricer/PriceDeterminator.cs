3a6dae1028659619cf50929a8ce8fc5558e10b97
Mohamed Salah <msalah@Mohameds-MacBook-Pro-2.local>
2020-01-24 01:04:49 +0200

    fist commit

---
+++ CarPricer/PriceDeterminator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace CarPricer
+{
+    
+    // WARNING: These instructions conflict with the instructions below.
+    //E.g., Start with the current value of the car,
+    // then adjust for age, take that result
+    // then adjust for miles,
+    // then collision, and
+    // finally previous owner. 
+    public class PriceDeterminator
+    {
+        const int MAX_COLLISIONS = 5;
+        const int MAX_AGE = 120;
+        const int MAX_MILES = 150000;
+        public decimal DetermineCarPrice(Car car)
+        {
+            if(car == null) throw new ArgumentException(nameof(car));
+            // WARNING: These instructions conflict with the instructions above.
+            //Each factor should be off of the result of the previous value in the order of
+            //    1. AGE
+            //    2. MILES
+            //    3. PREVIOUS OWNER
+            //    4. COLLISION
+            var value = car.PurchaseValue;
+            value = ApplyAgeReduction(car.AgeInMonths, value);
+            value = ApplyMilesReduction(car.NumberOfMiles, value);
+            //if previous owner, had a positive effect,
+            //then it should be applied AFTER step 4.
+            //If a negative effect, then BEFORE step 4.
+            value = ApplyPreviousOwnersReduction(car.NumberOfPreviousOwners, value);
+            value = ApplyCollisionReduction(car.NumberOfCollisions, value);
+            value = ApplyPreviousOwnersBonus(car.NumberOfPreviousOwners, value);
+
+            return value;
+        }
+
+        public decimal ApplyAgeReduction(int ageInMonths, decimal currentValue)
+        {
+            if(currentValue < 0) throw new ArgumentException(nameof(currentValue));
+            if(ageInMonths < 0) throw new ArgumentException(nameof(ageInMonths));
+            //Given the number of months of how old the car is,
+            //reduce its value one-half (0.5) percent.
+            //After 10 years, it's value cannot be reduced further by age.
+            //This is not cumulative.
+            if (ageInMonths > MAX_AGE) ageInMonths = MAX_AGE;
+            var percentage = 1 - (ageInMonths * 0.005m);
+            return currentValue * percentage;
+        }
+
+        public decimal ApplyMilesReduction(int numberOfMiles, decimal currentValue)
+        {
+            if(currentValue < 0) throw new ArgumentException(nameof(currentValue));
+            if(numberOfMiles < 0) throw new ArgumentException(nameof(numberOfMiles));
+            //For every 1,000 miles on the car,
+            //reduce its value by one-fifth of a percent(0.2).
+            //Do not consider remaining miles. After 150,000 miles,
+            //it's value cannot be reduced further by miles.
+            if (numberOfMiles > MAX_MILES) numberOfMiles = MAX_MILES;
+            var thousandsOfMiles = Math.Floor(numberOfMiles / 1000m);
+            var percentage = 1 - (thousandsOfMiles * 0.002m);
+            return currentValue * percentage;
+        }
+
+        public decimal ApplyPreviousOwnersReduction(int previousOwners, decimal currentValue)
+        {
+            if(currentValue < 0) throw new ArgumentException(nameof(currentValue));
+            if(previousOwners < 0) throw new ArgumentException(nameof(previousOwners));
+            //If the car has had more than 2 previous owners,
+            //reduce its value by twenty-five(25) percent.
+            if (previousOwners > 2) return currentValue * 0.75m;
+            return currentValue;
+        }
+
+        public decimal ApplyCollisionReduction(int numberOfCollisions, decimal currentValue)
+        {
+            if(currentValue < 0) throw new ArgumentException(nameof(currentValue));
+            if(numberOfCollisions < 0) throw new ArgumentException(nameof(numberOfCollisions));
+            //For every reported collision the car has been in, remove two (2) 
+            //percent of it's value up to five (5) collisions.
+            if (numberOfCollisions == 0) return currentValue;
+            if (numberOfCollisions > MAX_COLLISIONS) numberOfCollisions = MAX_COLLISIONS ;
+            var percentage = 1 - (numberOfCollisions * 0.02m);
+            return currentValue * percentage;
+        }
+        public decimal ApplyPreviousOwnersBonus(int previousOwners, decimal currentValue)
+        {
+            if(currentValue < 0) throw new ArgumentException(nameof(currentValue));
+            if(previousOwners < 0) throw new ArgumentException(nameof(previousOwners));
+            //If the car has had no previous owners,
+            //add ten(10) percent of the FINAL car value at the end.
+            if (previousOwners == 0) return currentValue * 1.1m;
+            return currentValue;
+        }
+    }
+}
---
+++ CarPricerTest/CarPricerTests.cs
@@ -0,0 +1,88 @@
+﻿using CarPricer;
+using NUnit.Framework;
+
+namespace CarPricerTest
+{
+
+    [TestFixture()]
+    public class UnitTests
+    {
+        [TestCase(24, 50000, 44000)]
+        [TestCase(120, 50000, 20000)]
+        [TestCase(150, 50000, 20000)]
+        public void Should_calculate_age_price_reduction(int age, decimal price, decimal expected)
+        {
+            var actual = new PriceDeterminator().ApplyAgeReduction(age, price);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1000, 50000, 49900)]
+        [TestCase(2000, 50000, 49800)]
+        [TestCase(2239, 50000, 49800)]
+        [TestCase(150000, 50000, 35000)]
+        [TestCase(200000, 50000, 35000)]
+        public void Should_calculate_miles_price_reduction(int miles, decimal price, decimal expected)
+        {
+            var actual = new PriceDeterminator().ApplyMilesReduction(miles, price);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, 50000, 50000)]
+        [TestCase(1, 50000, 50000)]
+        [TestCase(2, 50000, 50000)]
+        [TestCase(3, 50000, 37500)]
+        [TestCase(5, 50000, 37500)]
+        public void Should_calculate_previous_owners_price_reduction(int owners, decimal price, decimal expected)
+        {
+            var actual = new PriceDeterminator().ApplyPreviousOwnersReduction(owners, price);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, 50000, 50000)]
+        [TestCase(1, 50000, 49000)]
+        [TestCase(2, 50000, 48000)]
+        [TestCase(5, 50000, 45000)]
+        public void Should_calculate_collision_reduction(int collisons, decimal price, decimal expected)
+        {
+            var actual = new PriceDeterminator().ApplyCollisionReduction(collisons, price);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(3, 50000, 50000)]
+        [TestCase(2, 50000, 50000)]
+        [TestCase(1, 50000, 50000)]
+        [TestCase(0, 50000, 55000)]
+        public void Should_calculate_previous_owners_bonus(int owners, decimal price, decimal expected)
+        {
+            var actual = new PriceDeterminator().ApplyPreviousOwnersBonus(owners, price);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void CalculateCarValue()
+        {
+            AssertCarValue(25313.40m, 35000m, 3 * 12, 50000, 1, 1);
+            AssertCarValue(19688.20m, 35000m, 3 * 12, 150000, 1, 1);
+            AssertCarValue(19688.20m, 35000m, 3 * 12, 250000, 1, 1);
+            AssertCarValue(20090.00m, 35000m, 3 * 12, 250000, 1, 0);
+            AssertCarValue(21657.02m, 35000m, 3 * 12, 250000, 0, 1);
+        }
+
+        private static void AssertCarValue(decimal expectValue, decimal purchaseValue,
+        int ageInMonths, int numberOfMiles, int numberOfPreviousOwners, int
+        numberOfCollisions)
+        {
+            Car car = new Car
+            {
+                AgeInMonths = ageInMonths,
+                NumberOfCollisions = numberOfCollisions,
+                NumberOfMiles = numberOfMiles,
+                NumberOfPreviousOwners = numberOfPreviousOwners,
+                PurchaseValue = purchaseValue
+            };
+            PriceDeterminator priceDeterminator = new PriceDeterminator();
+            var carPrice = priceDeterminator.DetermineCarPrice(car);
+            Assert.AreEqual(expectValue, carPrice);
+        }
+    }
+}
